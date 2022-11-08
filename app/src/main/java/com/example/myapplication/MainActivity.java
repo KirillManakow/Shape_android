1989209bf4b509dd3b7459f28b69d7bf05944cb5
unknown <@ngknn.local>
2022-11-08 09:27:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //public void Add_bt(View v)
+    //{
+     //   Intent intent = new Intent(this, Add.class);
+       // startActivity(intent);
+    //}
+}
